c5246f0bb8581ca6167d6cfb549ba0e112cc14b9
Б.Сайнбаяр <sainbayar@erdenetmc.mn>
2025-08-11 16:03:01 +0800

    updtng

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Burger Builder</h1>
+        <p>Build your own delicious burger!</p>
       </header>
+      <main>
+        <section className="burger-builder">
+          {/* {<BurgerBuilder />} */}
+        </section>
+      </main>
+      <footer className="App-footer">
+        <p>&copy; 2023 Burger Builder. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
---
+++ src/component/burgerBuild/burger.js
@@ -0,0 +1,22 @@
+import './Burger.css';
+const Burger = () => {
+  return (
+    <>
+      <div className="burger">
+        <div className="top-bun"></div>
+        <div className="seed1"></div>
+        <div className="seed2"></div>
+        <div className="seed3"></div>
+      </div>
+
+      <div className="meat">Meat</div>
+      <div className="cheese">cheese</div>
+      <div className="salad">salad</div>
+      <div className="tomato">tomato</div>
+      
+      <div className="top-bun"></div>
+    </>
+  );
+};
+
+export default Burger;
